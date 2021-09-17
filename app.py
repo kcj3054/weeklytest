2d128aefe00adaca2c654b74d3818baaf885b77c
kcj3054 <kcj3054@naver.com>
2021-09-17 19:14:07 +0900

    haha

--- app.py
+++ app.py
@@ -13,22 +13,15 @@ db = client.dbStock
 def home():
    return render_template('index.html')
 
-@app.route('/stock', methods=['GET'])
-def getStock():
-
-    print(stock);
-    return jsonify({'all_stock': stock})
-
 ## API 역할을 하는 부분
 @app.route('/stock', methods=['POST'])
 def save_info():
-    info = request.json
+    jsonData = request.get_json();
     # db.collection.distinct(컬럼명)
     # market, sector, tag
-
     stock = db.codes.distinct('info')
     ##생략
-	return jsonify(stock)
+	return jsonify({})
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
